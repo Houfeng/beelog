d945edbaecc451c18f7abb8eaa148002fdb820db
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2023-12-16 16:01:26 +0800

    theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 module.exports = {
-  content: ["./views/**/*.html"],
+  content: ['./views/**/*.html'],
   plugins: [
-    require("daisyui"),
+    require('daisyui'),
     require('@tailwindcss/typography'),
   ],
   daisyui: {
     themes: [
-      "lofi",
-      "black",
+      {
+        light: {
+          ...require('daisyui/src/theming/themes')['lofi'],
+        },
+      },
+      {
+        dark: {
+          ...require('daisyui/src/theming/themes')['black'],
+          '--rounded-box': '0.25rem',
+          '--rounded-btn': '0.125rem',
+          '--rounded-badge': '0.125rem',
+          '--tab-radius': '0.125rem',
+          '--animation-btn': 0,
+          '--animation-input': 0,
+          '--btn-focus-scale': 1,
+        },
+      },
     ],
   },
   theme: {
     fontFamily: {
-      // 'FangsongGB2312', 'ui-serif', 'serif'
       serif: ['STFangsong', 'Fangsong', 'FangsongGB2312', 'ui-serif', 'serif'],
     },
   }
