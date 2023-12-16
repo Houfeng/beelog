229199d977aab18ca6bc749c8e22a7bab9b7b71e
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2023-12-16 22:06:57 +0800

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,8 +27,27 @@ module.exports = {
   },
   theme: {
     fontFamily: {
-      serif: ['STFangsong', 'Fangsong', 'FangsongGB2312', 'ui-serif', 'serif'],
-      sans: ['sans'],
+      serif: [
+        'STFangsong',
+        'Fangsong',
+        'FangsongGB2312',
+        'ui-serif',
+        'serif',
+      ],
+      sans: [
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial', 'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'],
     },
   }
 }
